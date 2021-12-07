e235295f7830f038f9d1af5989ee830acb2f1f27
Andrés Balaguera Antolínez <abalant@gmail.com>
2021-12-07 23:09:16 +0100

    Add files via upload

--- Main/cosmicatlass.cpp
+++ Main/cosmicatlass.cpp
@@ -1,12 +1,21 @@
-
 // ##################################################################################
-// ################################################################################# 
-// COSMICATLASS 
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+// COSMICATLASS
 // COSMologIcal
 // CATalogs for
 // LArge
 // Scale
 // Structure
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
 
 
 /** @file main_cosmicatlassu.cpp
@@ -18,7 +27,8 @@
 # include "../Headers/Bam.h"
 # include "../Headers/FileOutput.h"
 # include "../Headers/NumericalMethods.h"
-# include "Tasks.h"
+# include "../Headers/massFunctions.h"
+//# include "Tasks.h"
 
 using namespace std;
 
@@ -31,160 +41,135 @@ int main(int argc, char *argv[]){
   time(&start_all);
   char temp;
   string par_file_bam;
-  
 
-  ScreenOutput So(start_all);
-  
+  int NTHREADS=omp_get_max_threads();
+  omp_set_num_threads(NTHREADS);
+
+  string logfile="logfile.log";
+  ScreenOutput So(start_all, logfile);
+
   if(argc==1){
     So.usage(argv[0]);
     exit(1);
   }
-  while((temp =  getopt(argc, argv, "hiam:n:c:t:p:x:r:")) != -1)
+  while((temp =  getopt(argc, argv, "hadi:b:p:c:t:u:f:")) != -1)
     {
       if(temp=='h')
-	So.usage(argv[0]);
-      
+        So.usage(argv[0]);
+
       else if (temp=='a')  // Show authors
         So.author();
-      
-      else if(temp=='c') // Run cosmicatlas 
-	{  
 
-	  So.message(start_all);      
-          par_file_bam = argv[2];
-	  Params Par(par_file_bam); 
-	  Bam bam(Par); 
-	  bam.So=So;
-          bam.bamrunner();
-	  So.message_time(start_all);
-	}
       else if(temp=='i') // displays input parameters
-	{  
-	  par_file_bam = argv[2];
-	  Params params(par_file_bam); 
-	  Bam bam(params);
-	  bam.show_params();
-	}
-      
-      else if(temp=='m')   // to Mesure power spectrum
-	{
-	  So.message(start_all);
-	  par_file_bam = argv[2];
-	  Params params(par_file_bam);
-	  PowerSpectrumF cPSF(params);
-          cPSF.compute_power_spectrum(true,true);
-	  So.message_time(start_all);
-	}
-      else if(temp=='t')   // to read input tracer catalg and analyze it. The operations here should be included in BAM
-	{
-	  So.message(start_all);
-	  par_file_bam = argv[2];
-	  Params params(par_file_bam);
-          PowerSpectrumF cPSF(params);
-
-          /*
-          Catalog tracer(params, "tracer");
-	  tracer.read_catalog(params._file_catalogue());
-	  // vector<real_prec>dummy(params._NGRID(),0);
-	  // tracer.get_number_density_field_grid(bool weight_mass=false, dummy);
-	  tracer.get_mass_function();
-	  So.message_time(start_all);
-*/
-	}
-      
-
-      else if(temp=='r')   // to read input tracer catalg and analyze it. The operations here should be included in BAM
-	{
-	  // string par_file = "parameters_cosmolib.ini";
-	  // CosmoLib Clib(par_file);
-	  // Clib.feed_cosmo_struct();
-	  // Clib.get_cosmolib();
-	  
-	}
-      
-      
-      else if('x'==temp)
-          {
-          // to make various tasks
-
+        {
           par_file_bam = argv[2];
           Params params(par_file_bam);
-          ULONG NTT=params._Nft()*params._Nft()*params._Nft();
-          string fileo=params.Output_directory;
-          string filen=params.Output_directory;
-          for(int j=0;j<6;++j)
-          {
-            vector<real_prec>dm(NTT,0);
-            real_prec expo=1./9.;
-            FileOutput File;
-            ofstream aou;
-            switch(j)
-             {
-              case(0):
-                File.read_array(params.Output_directory+"INVARIANT_TIDAL_I_original.dat", dm);
-                fileo=params.Output_directory+"Tinv1.txt";
-                filen=params.Output_directory+"Tinv1_new";
-               break;
-              case(1):
-                File.read_array(params.Output_directory+"INVARIANT_TIDAL_II_original.dat", dm);
-                fileo=params.Output_directory+"Tinv2.txt";
-                filen=params.Output_directory+"Tinv2_new";
-              break;
-              case(2):
-                File.read_array(params.Output_directory+"INVARIANT_TIDAL_III_original.dat", dm);
-                fileo=params.Output_directory+"Tinv3.txt";
-                filen=params.Output_directory+"Tinv3_new";
-             break;
-             case(3):
-                File.read_array(params.Output_directory+"INVARIANT_SHEAR_I_original.dat", dm);
-                fileo=params.Output_directory+"Vinv1.txt";
-                filen=params.Output_directory+"Vinv1_new";
-              break;
-             case(4):
-               File.read_array(params.Output_directory+"INVARIANT_SHEAR_II_original.dat", dm);
-               fileo=params.Output_directory+"Vinv2.txt";
-               filen=params.Output_directory+"Vinv2_new";
-             break;
-             case(5):
-                File.read_array(params.Output_directory+"INVARIANT_SHEAR_III_original.dat", dm);
-               fileo=params.Output_directory+"Vinv3.txt";
-               filen=params.Output_directory+"Vinv3_new";
-             break;
-            }
-          int Nbins=200;
-//          get_overdens(dm,dm);
-          vector<real_prec>hist(Nbins,0);
-          real_prec delta=2./static_cast<real_prec>(Nbins);
-#pragma omp parallel for
-          for(ULONG i=0;i<NTT;++i)
-            {
-              real_prec signx=sign(dm[i]);
-              dm[i]=signx*pow(abs(dm[i]), expo);
-            }
-          real_prec xmin=get_min(dm);
-          real_prec xmax=get_max(dm);
-          for(ULONG i=0;i<NTT;++i)
-            {
-              dm[i]=2.*(dm[i]-xmin)/(xmax-xmin)-1.0;
-              hist[get_bin(dm[i],-1.0, Nbins, delta,true)]++;
-            }
-          File.write_array(filen,dm);
-          aou.open(fileo.c_str());
-          real_prec maxh=get_max(hist);
-          for(int i=0;i<Nbins;++i)
-             aou<<-1.0+(i+0.5)*delta<<"  "<<hist[i]/static_cast<real_prec>(maxh)<<endl;
-          aou.close();
-
+          params.show_params();
+        }
+      else if(temp=='d') // displays input parameters
+        {
+          So.show_preproc();
+        }
+      else if(temp=='b') // Run BAM
+        {
+#if !defined _POWER__
+          So.message(start_all);
+          par_file_bam = argv[2];
+          Params Par(par_file_bam);
+          Bam bam(Par,true);
+          bam.set_So(So);
+          bam.bamrunner();
+#else
+          So.message_warning("_POWER_ is defined in def.h.");
+         exit(0);
+#endif
           }
 
-      }
+      else if(temp=='p')   // to mesure Power spectrum
+        {
+          So.message(start_all);
+          par_file_bam = argv[2];
+          Params params(par_file_bam);
+          PowerSpectrumF cPSF(params);
+	        if(params._measure_cross()==true && params._input_type()!="catalog")
+	          cPSF.compute_cross_power_spectrum_grid(true, params._delta_grid_file(), params._delta_grid_file2());
+	        else if(params._measure_cross()==true && params._input_type()=="catalog")
+	          cPSF.compute_power_spectrum(false,false);
+	        else if(params._measure_cross()==false && ( params._input_type()=="density_grid" || params._input_type()=="delta_grid" ) )
+	          cPSF.compute_power_spectrum_grid();
+          else if(params._measure_cross()==false && params._input_type()=="catalog")
+            cPSF.compute_power_spectrum(true,false);
 
-//          tasks(argv[2]);
-      
+#ifdef _USE_REDSHIFT_BINS_
+          if(params._sys_of_coord_g()==0)
+          {
+              So.message_warning("Asking for redshift bins while providing cartessian coordinates. Check input parameter file and def.h");
+              exit(0);
+          }
+#endif
+
+          So.message_time(start_all);
+        }
+      else if(temp=='c')   // to read input tracer catalg and analyze it. 
+        {
+
+          So.message_screen("cosmicatlass running under the option -c");
+#ifdef _REDSHIFT_SPACE_
+          So.message_warning("Option _REDSHIFT_SPACE_ in def.h is enabled. If velocities are not to be used, please desable it to save space. Code ends here.");
+          So.message_screen("After editing the def.h file, please do make clean; make -j bam;");
+          exit(0);
+#endif
+          So.message(start_all);
+          par_file_bam = argv[2];
+          Params params(par_file_bam);
+          Bam bam(params,false);
+          Catalog catalog(params); // this is redundant
+          catalog.analyze_cat();
+          So.message_time(start_all);
+        }
+
+      else if(temp=='f')   // convert the xyz binary files from fastpm to a density field. Mianly used for IC
+        {
+/*          So.message(start_all);
+          par_file_bam = argv[2];
+          Params params(par_file_bam);
+          Catalog tracer(params, "TRACER");
+          tracer.read_catalog_bin();*/
+          }
+      else if(temp=='t')   // Run Patchy
+        {
+
+        }
+
+      else if(temp=='m')   // Run Patchy
+        {
+        }
+
+
+      else if('u'==temp)  // to make various tasks
+        {
+              //          tasks(argv[2]);
+        }
+      else if('?'==temp)
+       {
+         cout<<endl;
+         cout<<"Argument not recognized."<<endl;
+         cout<<"Please run ./cosmicatlass.exe -h."<<endl;
+         exit(1);
+       }
+
     }
-  
-  
+  exit(0);
   return 0;
+
 }
-  
-  
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+// ##################################################################################
+// #################################################################################
+
+
